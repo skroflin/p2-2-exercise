f65312a9338106209c7f86f95160da6503759116
Sven Kroflin (jer je profesor prigovarao na imena) <sven.kroflin@gmail.com>
2025-06-06 16:53:54 +0200

    Dokumentacija
    Dodavanje dokumentacije u StudentProstorija kontroleru!

--- src/main/java/ffos/skroflin/controller/StudentProstorijaController.java
+++ src/main/java/ffos/skroflin/controller/StudentProstorijaController.java
@@ -9,6 +9,14 @@ import ffos.skroflin.model.StudentProstorija;
 import ffos.skroflin.model.dto.StudentProstorijaDTO;
 import ffos.skroflin.service.StudentPolicaService;
 import ffos.skroflin.service.StudentProstorijaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +31,11 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author svenk
  */
+@Tag(name = "Student -> Prostorija", description = "Dostupne rute za entitet StudentProstorija")
 @RestController
 @RequestMapping("/api/skroflin/studentProstorija")
 public class StudentProstorijaController {
+
     private final StudentProstorijaService prostorijaService;
     private final StudentPolicaService policaService;
 
@@ -33,92 +43,122 @@ public class StudentProstorijaController {
         this.prostorijaService = prostorijaService;
         this.policaService = policaService;
     }
-    
+
+    @Operation(
+            summary = "Dohvaća sve prostorije", tags = {"get", "prostorija"},
+            description = "Dohvaća sve prostorije sa svim podacima"
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = StudentProstorija.class)))),
+                @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+            })
+
     @GetMapping("/get")
-    public ResponseEntity get(){
+    public ResponseEntity get() {
         try {
             return new ResponseEntity<>(prostorijaService.getAll(), HttpStatus.FOUND);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+        @Operation(
+            summary = "Dohvaća prostoriju po šifri",
+            description = "Dohvaća prostoriju po danoj šifri sa svim podacima. "
+            + "Ukoliko ne postoji prostorija za danu šifru vraća prazan odgovor",
+            tags = {"prostorija", "getBy"},
+            parameters = {
+                @Parameter(
+                        name = "sifra",
+                        allowEmptyValue = false,
+                        required = true,
+                        description = "Primarni ključ prostorije u bazi podataka, mora biti veći od nula",
+                        example = "2"
+                )})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = StudentProstorija.class), mediaType = "application/json")),
+        @ApiResponse(responseCode = "204", description = "Ne postoji student za danu šifru", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "400", description = "Šifra mora biti veća od nula", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+    })
     
     @GetMapping("/getBySifra")
     public ResponseEntity getBySifra(
             @RequestParam int sifra
-    ){
+    ) {
         try {
             if (sifra <= 0) {
-                return new ResponseEntity<>("Šifra mora biti veća od 0!" +  " " + sifra, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>("Šifra mora biti veća od 0!" + " " + sifra, HttpStatus.BAD_REQUEST);
             }
-            
+
             StudentProstorija prostorija = prostorijaService.getBySifra(sifra);
             if (prostorija == null) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            
+
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @GetMapping("/getPoliceUProstoriji")
     public ResponseEntity getPoliceUProstoriji(
             @RequestParam int sifra
-    ){
+    ) {
         try {
             if (sifra <= 0) {
                 return new ResponseEntity("Šifra prostorije ne smije biti manja o 0!" + " " + sifra, HttpStatus.BAD_REQUEST);
             }
-            
+
             StudentProstorija prostorija = prostorijaService.getBySifra(sifra);
             if (prostorija == null) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            
+
             List<StudentPolica> police = policaService.getPoliceUProstoriji(prostorija);
             if (police.isEmpty()) {
                 return new ResponseEntity<>("Nema polica u ovoj prostoriji!", HttpStatus.NO_CONTENT);
             }
-            
+
             return new ResponseEntity<>(police, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @GetMapping("/getProstorPoKabinetu")
     public ResponseEntity getProstorPoKabinetu(
             @RequestParam boolean kabinet
-    ){
+    ) {
         try {
             List<StudentProstorija> prostorije = prostorijaService.getProstorPoKabinetu(kabinet);
-            
+
             if (prostorije.isEmpty()) {
                 return new ResponseEntity<>("Nema prostorije s navedenim kriterijima!", HttpStatus.NO_CONTENT);
             }
-            
+
             return new ResponseEntity<>(prostorije, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @PostMapping("/post")
     public ResponseEntity post(
             @RequestBody(required = true) StudentProstorijaDTO dto
-    ){
+    ) {
         try {
             if (dto == null) {
                 return new ResponseEntity<>("podaci nisu primljeni", HttpStatus.BAD_REQUEST);
             }
             System.out.println("---->" + dto.naziv() + " " + dto.kabinet());
-            
+
             if (dto.naziv() == null || dto.naziv().isEmpty()) {
                 return new ResponseEntity<>("Naziv je obavezan!", HttpStatus.BAD_REQUEST);
             }
-            
+
             return new ResponseEntity<>(prostorijaService.post(dto), HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
